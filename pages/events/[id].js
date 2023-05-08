589e34d1af02f4a012f8f3af97feb4f449cf0f8d
Евгений Тюпа <evgenijtupa@MacBook-Air-Evgenij.local>
2023-05-09 00:03:57 +0300

    asd

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -19,7 +19,7 @@ const EventPage = (props) => {
                 <title>{event.title} | electroperedachi</title>
                 <meta name="description" lang="ua" content={event.description}/>
                 <meta name="description" lang="en" content={event.description_en}/>
-                <met name="keywords" content={`electroperedachi, ${event.keywords}`}/>
+                <met name="keywords" content={`electroperedachi`}/>
                 <meta property="og:image" content={event.poster.image}/>
             </Head>
             <EventPageComponent event={event} />
