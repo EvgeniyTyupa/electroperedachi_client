8d1ab2f383e0b79c86b81efe68401587604e9a9d
Евгений Тюпа <evgenijtupa@MacBook-Air-Evgenij.local>
2023-06-20 02:49:02 +0300

    remove bred

--- next.config.js
+++ next.config.js
@@ -12,20 +12,6 @@ const nextConfig = {
     domains: ['localhost', "89.116.236.29", "api.electroperedachi.com"]
   },
   distDir: 'build', 
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(mp4|webm)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          publicPath: '/_next',
-          name: 'static/videos/[name].[hash].[ext]',
-        },
-      },
-    });
-
-    return config;
-  },
 }
 
 module.exports = removeImports({
