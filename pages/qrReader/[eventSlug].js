b0a3976259fd48b35cbda1e41ea6144cab2ffed8
Евгений Тюпа <evgenijtupa@MacBook-Air-Evgenij.local>
2023-06-25 19:35:15 +0300

    bred

--- pages/qrReader/[eventSlug].js
+++ pages/qrReader/[eventSlug].js
@@ -7,6 +7,7 @@ import { useRef } from "react"
 import ActionButton from "../../components/UI/Buttons/ActionButton/ActionButton"
 import Head from "next/head"
 import { useAppContext } from "../../context/AppContext"
+import { useEffect } from "react"
 
 const QRCodeReader = (props) => {
     const { currentEvent } = props
@@ -46,9 +47,14 @@ const QRCodeReader = (props) => {
 
                 if (code) {
                     setIsFetchingContext(true)
-                    
+
                     const { _id, userId, eventId } = JSON.parse(code.data)
-                    const res = await eventApi.scanTicket(_id, userId, eventId, currentEvent._id)
+                    const res = await eventApi.scanTicket(
+                        _id,
+                        userId,
+                        eventId,
+                        currentEvent._id
+                    )
 
                     setResult(res)
 
@@ -69,12 +75,84 @@ const QRCodeReader = (props) => {
     }
 
     const handleClick = () => {
-        setImageSource('');
+        setImageSource("")
         if (inputRef.current) {
-            inputRef.current.value = null;
+            inputRef.current.value = null
         }
         inputRef.current.click()
     }
+
+    const videoRef = useRef(null)
+    const canvasRef = useRef(null)
+
+    useEffect(() => {
+        const constraints = { video: { facingMode: "environment" } }
+
+        const startCamera = async () => {
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia(
+                    constraints
+                )
+                videoRef.current.srcObject = stream
+            } catch (error) {
+                console.error("Error accessing camera:", error)
+            }
+        }
+
+        startCamera()
+
+        return () => {
+            // Stop the camera when the component unmounts
+            if (videoRef.current.srcObject) {
+                videoRef.current.srcObject.getTracks().forEach((track) => {
+                    track.stop()
+                })
+            }
+        }
+    }, [])
+
+    const processFrame = async () => {
+        const video = videoRef.current
+        const canvas = canvasRef.current
+        const context = canvas.getContext("2d")
+
+        context.drawImage(video, 0, 0, canvas.width, canvas.height)
+
+        const imageData = context.getImageData(
+            0,
+            0,
+            canvas.width,
+            canvas.height
+        )
+        const code = jsQR(imageData.data, imageData.width, imageData.height)
+
+        if (code) {
+            setIsFetchingContext(true)
+
+            const { _id, userId, eventId } = JSON.parse(code.data)
+            const res = await eventApi.scanTicket(
+                _id,
+                userId,
+                eventId,
+                currentEvent._id
+            )
+
+            setResult(res)
+
+            setIsFetchingContext(false)
+        } else {
+            setResult({
+                status: "bad",
+                message: "QR not found"
+            })
+        }
+
+        requestAnimationFrame(processFrame)
+    }
+
+    useEffect(() => {
+        requestAnimationFrame(processFrame)
+    }, [])
 
     return (
         <>
@@ -84,27 +162,8 @@ const QRCodeReader = (props) => {
             <div className={classes.qrReader}>
                 <Container className={classes.container}>
                     <div className={classes.qrBox}>
-                        {imageSource ? (
-                            <img
-                                src={imageSource}
-                                className={classes.previewStyle}
-                            />
-                        ) : (
-                            <div className={classes.emptyPreview} />
-                        )}
-                        <div className={classes.butContainer}>
-                            <input
-                                type="file"
-                                capture="camera"
-                                accept="image/*"
-                                hidden
-                                ref={inputRef}
-                                onChange={handleImageUpload}
-                            />
-                            <ActionButton onClick={handleClick} className={classes.submitQRBut}>
-                                Submit QR code
-                            </ActionButton>
-                        </div>
+                        <video ref={videoRef} autoPlay playsInline muted />
+                        <canvas ref={canvasRef} />
                     </div>
                     <div className={classes.qrOutput}>
                         {result ? (
@@ -129,7 +188,9 @@ const QRCodeReader = (props) => {
                                         }
                                     >
                                         {" "}
-                                        {result.status === "good" ? "good" : "bad"}
+                                        {result.status === "good"
+                                            ? "good"
+                                            : "bad"}
                                     </span>
                                 </p>
                                 <p>
@@ -137,7 +198,9 @@ const QRCodeReader = (props) => {
                                 </p>
                             </div>
                         ) : (
-                            <p className={classes.noData}>No data submited yet</p>
+                            <p className={classes.noData}>
+                                No data submited yet
+                            </p>
                         )}
                     </div>
                 </Container>
@@ -176,7 +239,7 @@ export async function getStaticProps(context) {
             notFound: true
         }
     }
-    
+
     return {
         props: {
             currentEvent: event
