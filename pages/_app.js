2ec233e17fc8621a144e828232e1594337ed13c2
EvgeniyTyupa <boogee2820@gmail.com>
2025-05-18 20:44:30 +0300

    asdasd

--- pages/_app.js
+++ pages/_app.js
@@ -135,22 +135,6 @@ export default function App({ Component, pageProps }) {
                                 data-type="two"
                                 data-position="bottom-left"
                             /> */}
-                            <Script
-                                id="hotjar"
-                                strategy="afterInteractive"
-                                dangerouslySetInnerHTML={{
-                                    __html: `
-                                      (function(h,o,t,j,a,r){
-                                        h.hj = h.hj || function(){(h.hj.q = h.hj.q || []).push(arguments)};
-                                        h._hjSettings = { hjid: 2742746, hjsv: 6 };
-                                        a = o.getElementsByTagName('head')[0];
-                                        r = o.createElement('script'); r.async = 1;
-                                        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-                                        a.appendChild(r);
-                                      })(window, document, 'https://static.hotjar.com/c/hotjar-','.js?sv=');
-                                    `,
-                                }}
-                            />
                         </Head>
                         <LocalizationProvider dateAdapter={AdapterMoment}>
                             <AnimatePresence
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,24 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <script
+          id="hotjar"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                h.hj = h.hj || function(){(h.hj.q = h.hj.q || []).push(arguments)};
+                h._hjSettings = { hjid: 2742746, hjsv: 6 };
+                a = o.getElementsByTagName('head')[0];
+                r = o.createElement('script'); r.async = 1;
+                r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window, document, 'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
